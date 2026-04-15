137896e02a54d8ac7a14dcd0c0ffde9d939a148b
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2026-04-15 09:51:22 +0200

    lab06

---
+++ Infrastructure/EntityFramework/Repositories/EfPersonRepository.cs
@@ -0,0 +1,20 @@
+using AppCore.Entities;
+using AppCore.Repositories;
+using Infrastructure.EntityFramework.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EntityFramework.Repositories;
+
+public class EfPersonRepository(ContactsDbContext context)
+    : EfGenericRepository<Person>(context.People), IPersonRepository
+{
+    public async Task<IEnumerable<Person>> GetByEmployerAsync(Guid companyId) =>
+        await context.People
+            .Where(p => p.Employer != null && p.Employer.Id == companyId)
+            .ToListAsync();
+
+    public async Task<IEnumerable<Person>> GetByOrganizationAsync(Guid organizationId) =>
+        await context.People
+            .Where(p => p.Organization != null && p.Organization.Id == organizationId)
+            .ToListAsync();
+}
---
+++ Infrastructure/EntityFramework/UnitOfWork/EfContactsUnitOfWork.cs
@@ -0,0 +1,28 @@
+using AppCore.Repositories;
+using Infrastructure.EntityFramework.Context;
+
+namespace Infrastructure.EntityFramework.UnitOfWork;
+
+public class EfContactsUnitOfWork(
+    IPersonRepository personRepository,
+    ICompanyRepository companyRepository,
+    IOrganizationRepository organizationRepository,
+    ContactsDbContext context) : IContactUnitOfWork
+{
+    public IPersonRepository Persons => personRepository;
+    public ICompanyRepository Companies => companyRepository;
+    public IOrganizationRepository Organizations => organizationRepository;
+
+    public Task<int> SaveChangesAsync() => context.SaveChangesAsync();
+
+    public Task BeginTransactionAsync() =>
+        context.Database.BeginTransactionAsync();
+
+    public Task CommitTransactionAsync() =>
+        context.Database.CommitTransactionAsync();
+
+    public Task RollbackTransactionAsync() =>
+        context.Database.RollbackTransactionAsync();
+
+    public ValueTask DisposeAsync() => context.DisposeAsync();
+}
